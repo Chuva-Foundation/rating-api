a68b81a9a889bac8f33c2bb0a02562ccfa25c98a
Vandro Neves dos Santos <vandro.nds@gmail.com>
2023-08-11 17:16:36 -0100

    #27: When a review is created it should publish to the socket

---
+++ src/topics/rating_created/notify/index.js
@@ -0,0 +1,10 @@
+const { sockets } = require('@chuva.io/less');
+const Connection = require('models/Connection');
+
+exports.process = async (rating) => {
+    const connections = await Connection.findAll();
+
+    const connection_ids = connections.map(connection => connection.connection_id);
+
+    await sockets.review.publish(rating, [connection_ids]);
+};
